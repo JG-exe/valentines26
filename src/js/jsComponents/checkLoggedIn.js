342faddf953f17a4a740acc3cc96bbea36da2285
Joachim <joachim.gautama@student.ehb.be>
2026-02-14 19:31:37 +0100

    update

--- src/js/jsComponents/checkLoggedIn.js
+++ src/js/jsComponents/checkLoggedIn.js
@@ -1,5 +1,7 @@
 const validation = JSON.parse(sessionStorage.getItem("ok"));
 const didShe = JSON.parse(localStorage.getItem("didShe"));
 
+console.log(didShe);
+
 if(!validation) window.location = "./"
 if(didShe) window.location = "./yay"
